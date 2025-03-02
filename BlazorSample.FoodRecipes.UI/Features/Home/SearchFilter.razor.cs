bff264b216c70ec52c612293c2ada53ff02a7e68
Mohammadreza Hashemi <it93mohammad@gmail.com>
2025-03-02 14:46:38 +0330

    New: SearchFilter was added

---
+++ BlazorSample.FoodRecipes.UI/Features/Home/SearchFilter.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorSample.FoodRecipes.UI.Features.Home;
+
+public partial class SearchFilter
+{
+    [Parameter]
+    public int MaxTime { get; set; }
+    [Parameter, EditorRequired]
+    public string SearchTerm { get; set; } = string.Empty;
+
+    private void Filter()
+    {
+        var myNewAddress = _navigationManager.GetUriWithQueryParameter("MaxTime", MaxTime);
+        _navigationManager.NavigateTo(myNewAddress);
+    }
+
+    private void Clear()
+    {
+        MaxTime = 0;
+        _navigationManager.NavigateTo($"/search/{SearchTerm}");
+    }
+}
---
+++ BlazorSample.FoodRecipes.UI/Features/Home/SearchPage.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorSample.FoodRecipes.UI.Features.Home;
+
+public partial class SearchPage
+{
+    [Parameter]
+    public string SearchTerm { get; set; } = string.Empty;
+    [Parameter, SupplyParameterFromQuery]
+    public int MaxTime { get; set; }
+
+    public IEnumerable<Recipe> _recipes { get; set; }
+    public Recipe selectedRecipe { get; set; }
+    protected async override Task OnInitializedAsync()
+    {
+        try
+        {
+            _recipes = (await _httpClient.GetFromJsonAsync<IEnumerable<Recipe>>("Data.json"))
+                .Where(w => w.Name.Contains(SearchTerm) || w.Description.Contains(SearchTerm))
+                .Where(a => MaxTime == 0 || a.TimeInMinutes <= MaxTime);
+
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Badness => {ex.Message}");
+        }
+    }
+
+    void OnSelectRecipeHandler(Recipe recipe)
+    {
+        selectedRecipe = recipe;
+    }
+}
